c070d3fbc3f85c061bae64979cbacad222b0c66d
ZedStack <agusvali97@gmail.com>
2015-07-09 02:15:10 -0300

    Testing the library

--- v1.5/Files/Draw.h
+++ v1.5/Files/Draw.h
@@ -1,3 +1,17 @@
+/*┌──────────────────────────────────────────────────────────────────────────────────┐
+┌─┼─┐  Welcome to...                                                               ┌─┼─┐
+│ └─┘                                                                              └─┘ │
+│     ███████╗███████╗██████╗ ███████╗████████╗ █████╗  ██████╗██╗  ██╗   ██╗  ██╗     │
+│     ╚══███╔╝██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝   ██║  ██║     │
+│       ███╔╝ █████╗  ██║  ██║███████╗   ██║   ███████║██║     █████╔╝    ███████║     │
+│      ███╔╝  ██╔══╝  ██║  ██║╚════██║   ██║   ██╔══██║██║     ██╔═██╗    ██╔══██║     │
+│     ███████╗███████╗██████╔╝███████║   ██║   ██║  ██║╚██████╗██║  ██╗██╗██║  ██║     │
+│     ╚══════╝╚══════╝╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝     │
+│ ┌─┐                                                                              ┌─┐ │
+└─┼─┘                                 ...a simple library for Win32 GUI projects.  └─┼─┘
+  └──────────────────────────────────────────────────────────────────────────────────┘*/
+// V 1.5
+
 #pragma once
 
 // Library: Draw.h
@@ -8,20 +22,20 @@
 // Extra libraries in the project.
 #include "Struct.h"
 
-void rect		(COORDS figure, HDC hdc);
-void roundRect	(COORDS figure, HDC hdc);
-void circ		(COORDS figure, HDC hdc);
-void elli		(COORDS figure, HDC hdc);
+void rect       (COORDS figure, HDC hdc);
+void roundRect  (COORDS figure, HDC hdc);
+void circ       (COORDS figure, HDC hdc);
+void elli       (COORDS figure, HDC hdc);
 
-void filledRect		(COORDS figure, HDC hdc, COLORREF color);
-void normalRect		(COORDS figure, HDC hdc, HPEN& hPen);
-void filledRoundRect(COORDS figure, HDC hdc, COLORREF color);
-void normalRoundRect(COORDS figure, HDC hdc, HPEN& hPen);
-void filledCirc		(COORDS figure, HDC hdc, COLORREF color);
-void normalCirc		(COORDS figure, HDC hdc, HPEN& hPen);
-void filledElli		(COORDS figure, HDC hdc, COLORREF color);
-void normalElli		(COORDS figure, HDC hdc, HPEN& hPen);
+void filledRect         (COORDS figure, HDC hdc, COLORREF color);
+void normalRect         (COORDS figure, HDC hdc, HPEN& hPen);
+void filledRoundRect    (COORDS figure, HDC hdc, COLORREF color);
+void normalRoundRect    (COORDS figure, HDC hdc, HPEN& hPen);
+void filledCirc         (COORDS figure, HDC hdc, COLORREF color);
+void normalCirc         (COORDS figure, HDC hdc, HPEN& hPen);
+void filledElli         (COORDS figure, HDC hdc, COLORREF color);
+void normalElli         (COORDS figure, HDC hdc, HPEN& hPen);
 
 void deleteFilledObjetFigure(HGDIOBJ orig, HDC hdc, HBRUSH& hBrush);
 void deleteNormalObjetFigure(HGDIOBJ orig, HDC hdc, HPEN& hPen);
-void deleteObjetLine		(HGDIOBJ orig, HDC hdc, HPEN& hPen);
+void deleteObjetLine        (HGDIOBJ orig, HDC hdc, HPEN& hPen);
